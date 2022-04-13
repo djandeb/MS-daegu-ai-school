1969c858aed9e813a9e47945a84e2a373f13db00
djandeb <103418672+djandeb@users.noreply.github.com>
2022-04-13 21:07:53 +0900

    Add files via upload

---
+++ web/python/2.data.py
@@ -0,0 +1,33 @@
+print(1)
+print(1.1)
+print(1+1)
+print(2-1)
+print(2*2)
+print(6/2)
+print(2**3)
+print(pow(3,2))
+import random
+print(random.random())
+
+#String
+print('Hello')
+print('HELLO')
+print('''
+hello
+world
+''')
+print(len('helloworld'))
+
+# change directory = cd 불러오기 위치변경
+
+# list 리스트 만들기
+member=['eunbi','amy','gongjoo']
+print(member[0])
+print(len(member))
+
+import random
+print(random.choice(member))
+
+score = [100,200,300]
+print(sum(score))
+
---
+++ web/python/5.pip.py
@@ -0,0 +1,7 @@
+import pandas
+house=pandas.read_csv('boston.csv')
+print(house)
+
+
+#이유는 모르겠는데 그냥 실행하면 안되고
+#python 5.pip.py 라고 해야됨
